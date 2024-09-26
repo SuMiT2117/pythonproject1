3f4fe935cfb17def91b8668b4ca6647b6cfaf78f
SuMiT2117 <suryawanshisumit2004@gmail.com>
2024-09-26 10:28:54 +0530

    Initial commit

---
+++ bandname.py
@@ -0,0 +1,5 @@
+print("Welcome to the band name generator!")
+city = input("what the name of the city you are grown ?")
+pet = input("what's your pet name ?")
+
+print("Your band name is :" +city +" "+pet)
